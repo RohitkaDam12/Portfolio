84cd7642e60bd7ab7e484234ea7a0b7bffe5cd61
Rohit Kadam <rohitkadam25635@gmail.com>
2025-05-02 02:51:19 +0530

    Use serve to run React app in production

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/RohitkaDam12",
   resume:
-    "",
+    "https://drive.google.com/file/d/1RvYW5hQfoAxbHCkS8Gghd6f8MVBeh2rH/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/rohitkadam04/",
   twitter: "",
   insta: "https://www.instagram.com/itsrohitkadam/",
